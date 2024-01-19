4f115ede1a5e07bd301fb275cee824aec7325541
Rainmaker519 <rainmaker519@gmail.com>
2024-01-19 00:42:54 -0500

    Updated functionality for building the network type iteratively by layer.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::f64::consts;
+use std::fs;
 
-#[derive(Copy, Clone, PartialEq)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 enum ActivationType {
     Sigmoid,
     Tanh,
@@ -27,7 +28,7 @@ impl PAVForwardPass for PreActivationValue {
     }
 }
 
-type Node = (PreActivationValue, Vec<Weight>, Vec<Weight>, usize, usize); // last 2 next and prev layer sizes
+type Node = (PreActivationValue, Vec<Weight>, Vec<Weight>, usize, usize, f64); // last 2 next and prev layer sizes + bias
 
 trait NodeForwardPass {
     fn get_nv_pairs(&self) -> Vec<(usize,(f64,f64))>;
@@ -80,6 +81,47 @@ impl MakeWeight for Weight {
 
 type Network = (Vec<Layer>,Vec<usize>);
 
+trait NetworkBuild {
+    fn start_network(input_layer_size: usize) -> Network;
+    fn add_layer(&mut self, l_chunk: ((Vec<Vec<Weight>>,Vec<f64>),(Vec<Vec<Weight>>,Vec<f64>)));
+}
+impl NetworkBuild for Network {
+    fn start_network(input_layer_size: usize) -> Network {
+        let input_layer: Layer = (vec![], input_layer_size);
+        let network: Network = (vec![input_layer], vec![input_layer_size]);
+        network
+    }
+    fn add_layer(&mut self, l_chunks: ((Vec<Vec<Weight>>, Vec<f64>),(Vec<Vec<Weight>>,Vec<f64>))) {
+        let weights_0 = l_chunks.0.0;
+        let weights_1 = l_chunks.1.0;
+        let biases = l_chunks.0.1;
+        let mut nodes: Vec<Node> = vec![];
+        let mut in_node_w_holder = vec![];
+
+        for i in 0..weights_1.len() {
+            let mut in_bank = vec![];
+            for w in weights_0.iter() {
+                in_bank.push(w[i]);
+            }
+            in_node_w_holder.push(in_bank);
+        }
+
+        for i in 0..weights_1.len() {
+            nodes.push((
+                (0f64,ActivationType::Sigmoid),
+                in_node_w_holder.clone()[i].clone(),
+                weights_1[i].clone(),
+                weights_0.len(),
+                weights_1.len(),
+                biases[i]
+            ));
+        }
+
+        self.0.push((nodes, weights_1.len()));
+        //checked and weights connected correctly according to test in main
+    }
+}
+
 
 fn main() {
     let v1: PreActivationValue = (0.0443, ActivationType::Sigmoid);
@@ -87,7 +129,7 @@ fn main() {
     let n1: Node = (v1,
          vec![Weight::make(0.12), Weight::make(0.16), Weight::make(0f64)],
          vec![Weight::make(0f64), Weight::make(0.0334), Weight::make(0.432)],
-        3, 3,
+        3, 3, 1.3
     );
 
     let nv_pairs = n1.get_nv_pairs();
@@ -96,6 +138,39 @@ fn main() {
         dbg!(i.0, i.1.0, i.1.1);
         println!();
     }
+
+    //let file: &str = "test_network.txt";
+    //let file = fs::read_to_string(file);
+
+    let layer_sizes: Vec<usize> = vec![4,2,4,5];//read from file when fr
+
+    let mut layers = Network::start_network(layer_sizes[0]);
+
+    let mut w_chunks = vec![
+        vec![Weight::make(0.2), Weight::make(0.4)],
+        vec![Weight::make(0.3), Weight::make(0.3)],
+        vec![Weight::make(0.2), Weight::make(0.4)],
+        vec![Weight::make(0.3), Weight::make(0.3)],
+    ];
+    let mut bias_chunk = vec![1.2,-0.83];
+    let l_chunk: (Vec<Vec<Weight>>,Vec<f64>) = (w_chunks,bias_chunk);
+
+    let mut w_chunks1 = vec![
+        vec![Weight::make(0.2), Weight::make(0.4),Weight::make(0.3), Weight::make(0.3)],
+        vec![Weight::make(0.3), Weight::make(0.3),Weight::make(0.2), Weight::make(0.4)],
+    ];
+    let mut bias_chunk1 = vec![1.2,-0.83,1.2,-0.83];
+    let l_chunk1: (Vec<Vec<Weight>>,Vec<f64>) = (w_chunks1,bias_chunk1);
+
+    layers.add_layer((l_chunk,l_chunk1));
+    dbg!(&layers.0[1].0[0]);
+
+    //add_layer takes two l_chunks
+    //an l_chunk represents the weights from the prev layers nodes, the first ind being the node in the prev layer
+    //  and the second ind being the node in this layer/
+    //in addition an l_chunk has the bias of the layer, the first of the two passed will be used for
+    //  the layer being added to the network
+
 
     println!("Hi test commit!");
 }
